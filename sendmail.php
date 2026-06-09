d741fe2c7533f6aea54553813a53c5170abc0111
Abdulla Al Kayes <abdullahalkayes59@gmail.com>
2026-06-09 14:47:48 +0600

    added new

--- sendmail.php
+++ sendmail.php
@@ -1,27 +1,59 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-    $to = "abdullahalkayes59@gmail.com"; // তোমার Gmail বসাও
-    $subject = "New Contact Message from Website";
+    $phpmailer = new PHPMailer(true);
 
-    $body = "Name: $name\n";
-    $body .= "Email: $email\n";
-    $body .= "Message: $message\n";
+    try {
 
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+        // SMTP setup
+        $phpmailer->isSMTP();
+        $phpmailer->Host       = 'sandbox.smtp.mailtrap.io';
+        $phpmailer->SMTPAuth   = true;
+        $phpmailer->Port       = 2525;
 
-    if (mail($to, $subject, $body, $headers)) {
-        header("Location: index.html?status=success");
-        exit();
-    } else {
-        header("Location: index.html?status=error");
-        exit();
+        // Mailtrap credentials
+        $phpmailer->Username   = '2e4d4780e41f3a';
+        $phpmailer->Password   = 'bbb4e3f6fdcbe9';
+
+        // Email settings
+        $phpmailer->setFrom('noreply@portfolio.com', 'Portfolio Website');
+        $phpmailer->addAddress('abdullahalkayes59@gmail.com');
+
+        $phpmailer->Subject = "New Contact Form Message";
+
+        $phpmailer->Body =
+        "New message from website:\n\n" .
+        "Name: $name\n" .
+        "Email: $email\n\n" .
+        "Message:\n$message";
+
+        $phpmailer->send();
+
+        echo "<script>
+                alert('Message Sent Successfully (Mailtrap)');
+                window.location='index.html';
+              </script>";
+
+    } catch (Exception $e) {
+
+        echo "<script>
+                alert('Message Failed!');
+                window.location='index.html';
+              </script>";
+
+        echo "Error: " . $phpmailer->ErrorInfo;
     }
 }
-
 ?>
